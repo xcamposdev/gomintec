a8795eea325f8b95f9543ed04c950998e44e65b4
xavi <xcampos@develoop.net>
2020-07-21 17:57:23 +0200

    [ADD] cambios crm, stock, sale, web, account

---
+++ custom_modules/crm_custom/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "crm_custom",
+
+    'summary': """
+        CRM custom""",
+
+    'description': """
+        CRM custom
+    """,
+
+    'author': "Develoop Software",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base_setup',
+        'sales_team',
+        'mail',
+        'calendar',
+        'resource',
+        'fetchmail',
+        'utm',
+        'web_tour',
+        'contacts',
+        'digest',
+        'phone_validation',
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
